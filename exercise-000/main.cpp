3cf33cc35760e1827c12ed9b5d3f90c70ee8d69f
Mark <129627276+MarkS2004@users.noreply.github.com>
2025-10-17 13:31:04 +0200

    solution to exercise

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -3,16 +3,39 @@
 
 #include "config.h"
 
+void changeValueByParameter(int value)
+{
+    value = 99;
+    fmt::println("In function: value = {}", value);
+}
+
+
+void changeValueByPointer(int* ptr)
+{
+    * ptr = 77;
+    fmt::println("In function: ptr = {}", *ptr);
+}
+
 int main(int argc, char **argv) {
 
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
-    fmt::print("Hello, {}!\n", tea::PROJECT_NAME);
+    double* ptr = new double(3.1415);
+    fmt::println("value: {}, address: {}", *ptr, fmt::ptr(ptr));
 
-    /* INSERT YOUR CODE HERE */
+    delete ptr;
+    fmt::println("value: {}, address: {}", *ptr, fmt::ptr(ptr));
+
+
+
+    int y = 5;
+    fmt::println("Before function: y = {}", y);
+    changeValueByParameter(y);
+    fmt::println("After function: y = {}\n", y);
+
+    fmt::println("Before function: y = {}", y);
+    changeValueByPointer(&y);
+    fmt::println("After function: y = {}", y);
+
+
 
     return 0; /* exit gracefully*/
 }
